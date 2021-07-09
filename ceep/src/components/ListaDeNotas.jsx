9a0de18dc4c61fc1178fd1242155d4ca1ce23e51
Luis A. Ruiz <nessiuscaffe@gmail.com>
2021-07-09 17:47:33 -0300

    adicionando classes e criando separação de componentes e pastas

--- ceep/src/components/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import CardNota from './CardNota';
+import CardNota from './CardNota/CardNota';
 
 
  class ListaDeNotas extends Component{
     
     render() {
         return(
-        <ul>
-            <li>
-                <CardNota/>
-            </li>
-            <li>
-                <CardNota/>
-            </li>
-            <li>
-                <CardNota/>
-            </li>
-        </ul>
-          
-          );
-   }
+            <ul>
+         {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+            return(
+                <li key={index}>
+                    <div>{categoria}</div>
+                    <CardNota/>
+                </li>
+                );
+              })}
+           </ul>
+         );
+    }
 }
 
 export default ListaDeNotas;
