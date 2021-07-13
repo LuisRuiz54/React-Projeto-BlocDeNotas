12d6ae69a40ba563e149cc62c3bd173e0e9ec3be
Luis A. Ruiz <nessiuscaffe@gmail.com>
2021-07-13 16:36:19 -0300

    Adicionando categorias dinamicas para minhas notas

--- ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./estilo.css";
+
 class ListaDeCategorias extends Component {
   _handleEventoInput(e) {
-    if (e.key == "Enter") {
+    if (e.key === "Enter") {
       let valorCategoria = e.target.value;
       this.props.adicionarCategoria(valorCategoria);
     }
